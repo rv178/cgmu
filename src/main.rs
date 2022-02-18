a3929d2175b7dcc16f25c1d44c87032ca46bd416
Idlidev <idliyout@gmail.com>
2022-02-18 20:49:42 +0530

    [update] it works now

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-//use std::process::Command;
+use nix::unistd::Uid;
+use std::process::Command;
 use youchoose;
 
 fn main() {
-    menu();
+    if !Uid::current().is_root() {
+        println!("CGMU must be run as root!");
+    } else {
+        menu();
+    }
 }
 
 fn menu() {
@@ -15,12 +20,32 @@ fn menu() {
     let choice: usize = choice[0];
 
     if choice == 0 {
+        let _output = Command::new("sh")
+            .arg("-c")
+            .arg("echo 'performance' | tee /sys/devices/system/cpu/*/cpufreq/scaling_governor")
+            .output()
+            .expect("Failed to execute command");
         println!("User chose 'performance'.");
     } else if choice == 1 {
+        let _output = Command::new("sh")
+            .arg("-c")
+            .arg("echo 'ondemand' | tee /sys/devices/system/cpu/*/cpufreq/scaling_governor")
+            .output()
+            .expect("Failed to execute command");
         println!("User chose 'ondemand'.");
     } else if choice == 2 {
+        let _output = Command::new("sh")
+            .arg("-c")
+            .arg("echo 'schedutil' | tee /sys/devices/system/cpu/*/cpufreq/scaling_governor")
+            .output()
+            .expect("Failed to execute command");
         println!("User chose 'schedutil'.");
     } else if choice == 3 {
+        let _output = Command::new("sh")
+            .arg("-c")
+            .arg("echo 'powersave' | tee /sys/devices/system/cpu/*/cpufreq/scaling_governor")
+            .output()
+            .expect("Failed to execute command");
         println!("User chose 'powersave'.");
     } else {
         println!("Your choice was {}.", choice);
